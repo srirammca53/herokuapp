761b92de93ba87f8af6296929db53e192912e9b3
sriram <sriram.in20@gmail.com>
2011-07-30 18:44:36 +0530

    latest

--- app/models/ckeditor/attachment_file.rb
+++ app/models/ckeditor/attachment_file.rb
@@ -1,7 +1,11 @@
 class Ckeditor::AttachmentFile < Ckeditor::Asset
   has_attached_file :data,
                     :url => "/ckeditor_assets/attachments/:id/:filename",
-                    :path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
+                    :storage => :s3,
+                    :s3_credentials => "#{RAILS_ROOT}/config/s3.yml",
+                    :bucket => "sharmanyros",
+                    :path => "/sriram/files/:style/:id/:filename" ;
+                    #:path => ":rails_root/public/ckeditor_assets/attachments/:id/:filename"
   
   validates_attachment_size :data, :less_than=>100.megabytes
   
